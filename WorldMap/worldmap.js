734629e62240f22609c63d4b2d276ffc8efcf09c
lunctis-viribus <>
2018-11-29 21:54:57 +0000

    Scatterplot is using the data

--- WorldMap/worldmap.js
+++ WorldMap/worldmap.js
@@ -1,6 +1,6 @@
 Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 
-var margin = {top: 20, right: 20, bottom: 20, left: 20};
+var margin = {top: 20, right: 20, bottom: 30, left: 40};
 var viewWidth = window.innerWidth / 2 - margin.right;
 var viewHeight = window.innerHeight - margin.bottom;
 
@@ -12,17 +12,23 @@ var zoom = d3.zoom()
 	 .translateExtent([[0, 0], [width, height]])
 	 .on("zoom", zoomed);
 
-var svg1 = d3.select("#map").append("svg")
+d3.select("#map").append("svg")
+	.attr("id", "svg1")
 	.attr("width", width)
 	.attr("height", height)
-	.attr("class","map")
 	.call(zoom);
-	
-var svg2 = d3.select("#comparison").append("svg")
-	.attr("width", width)
-	.attr("height", height)
-	.attr("class","comparison");
 
+var svg1 = d3.select("#svg1");
+
+d3.select("#comparison").append("svg")
+	.attr("id", "svg2")
+	.attr("width", width)
+	.attr("height", height);	
+
+var svg2 = d3.select("#svg2")
+	.append("g")
+		.attr("transform", "translate(" + 30 + "," + -25 + ")");
+		
 //for tooltip 
 var offsetL = document.getElementById('map').offsetLeft+10;
 var offsetT = document.getElementById('map').offsetTop+10;
@@ -53,115 +59,78 @@ var xValue = "x";
 var yValue = "y";
 var colorValue = "a";
 
-var svg2 = d3.select("svg2")
-    .attr("width", viewWidth)
-    .attr("height", viewHeight)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var defs = svg2.append( "defs" );
-
-var legendGradient = defs.append( "linearGradient" )
-    .attr( "id", "legendGradient" )
-    .attr( "x1", "0" )
-    .attr( "x2", "0" )
-    .attr( "y1", "1" )
-    .attr( "y2", "0" );
-
-legendGradient.append( "stop" )
-    .attr( "id", "gradientStart" )
-    .attr( "offset", "0%" )
-    .style( "stop-opacity", 1);
-
-legendGradient.append( "stop" )
-    .attr( "id", "gradientStop" )
-    .attr( "offset", "100%" )
-    .style( "stop-opacity", 1);
-
-var points;
-
 function drawScatterplot(d1, d2) {
-
-	data = [];
-	data.push(d1);
-	data.push(d2);
+	var data = [];
+	var xArray = Array.range(1961, 2017);
+	
+	for (var i = 0; i < d1.length; i++) {
+		var item = {"gdpGrowth1": d1[i],"gdpGrowth2": d2[i]};
+		data[i] = item;
+		data[i].gdpGrowth1 = d1[i];
+		data[i].gdpGrowth2 = d2[i];
+		data[i].x = xArray[i];
+	}
 	console.log("d1: " + d1);
 	console.log("d2: " + d2);
-	console.log("data: " + data);
+	console.log("data[0]: " + data[0]);
 
-	var xArray = Array.range(1961, 2017);
+	
 
-  var xExtent = d3.extent(xArray, function(d) { return d; });
-  var yExtent = d3.extent(data, function(d) { return d["gdpGrowth"]; });
+	var xExtent = d3.extent(xArray, function(d) { return d; });
+	var yExtent = d3.extent(data, function(d) { return d["gdpGrowth1"]; });
 
-  x.domain(xExtent).nice();
-  y.domain(yExtent).nice();
+	x.domain(xExtent).nice();
+	y.domain(yExtent).nice();
 
-  svg2.selectAll("g").remove();
+	svg2.selectAll("g").remove();
 
-  svg2.append("g")
-      .attr("id", "xAxis")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("id", "xLabel")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("x");
+	svg2.append("g")
+		.attr("id", "xAxis")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis)
+	.append("text")
+		.attr("class", "label")
+		.attr("id", "xLabel")
+		.attr("x", width)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+		.text("gdpGrowth");
 
-  svg2.append("g")
-      .attr("id", "yAxis")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("id", "yLabel")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("y");
+	svg2.append("g")
+		.attr("id", "yAxis")
+		.attr("class", "y axis")
+		.call(yAxis)
+	.append("text")
+		.attr("class", "label")
+		.attr("id", "yLabel")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("y");
 
-  points = svg2.append("g")
-      .attr("class", "plotArea")
-    .selectAll(".dot")
-      .data(data)
+	var points1 = svg2.append("g")
+		.attr("class", "plotArea")
+	.selectAll(".dot")
+		.data(data)
     .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return x(Array.range(1961, 2017)); })
-      .attr("cy", function(d) { return y(d["gdpGrowth"]); })
+		.attr("class", "dot")
+		.attr("class", "dotscountry1")
+		.attr("r", 3.5)
+		.attr("cx", function(d) { return x(d.x); })
+		.attr("cy", function(d) { return y(d["gdpGrowth1"]); })
 
-  svg2.select("#gradientStart")
-    .style("stop-color", colors[0]);
-  svg2.select("#gradientStop")
-    .style("stop-color", colors[1]);
-
-  var legend = svg2.append("g")
-      .attr("class", "legend");
-
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 72)
-      .style("fill", "url(#legendGradient)");
-
-  legend.append("text")
-      .attr("x", width - 22)
-      .attr("y", 6)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text("high");
-
-  legend.append("text")
-      .attr("x", width - 22)
-      .attr("y", 66)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text("low");
+	var points2 = svg2.append("g")
+		.attr("class", "plotArea")
+    .selectAll(".dot")
+		.data(data)
+    .enter().append("circle")
+		.attr("class", "dot")
+		.attr("class", "dotscountry2")
+		.attr("r", 3.5)
+		.attr("cx", function(d) { return x(d.x); })
+		.attr("cy", function(d) { return y(d["gdpGrowth2"]); })
 
 }
 
@@ -183,14 +152,21 @@ function getCountryData(c1, c2){
   	gdpGrowth : growthArray
   };
 }, function(data){
-	var compare = [];
-	data.forEach(function(element){
-		if(element.country_code === c1 || element.country_code === c2){
+	var country1 = 0;
+	var country2 = 0;
+	data.forEach(function(element, i){
+		if(element.country_code === c1){
 			console.log(element);
-			compare.push(element);
+			//compare.push(element);
+			country1 = i;
+		} else if (element.country_code === c2){
+			console.log(element);
+			//compare.push(element);
+			country2 = i;
 		}
 	})
-	drawScatterplot(compare[0], compare[1]);
+	//console.log(data[country1].gdpGrowth);
+	drawScatterplot(data[country1].gdpGrowth, data[country2].gdpGrowth);
 
 });
 }
