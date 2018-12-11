94f7c6b564496ceaa8df4588a7c4f0da95fb2580
olivier-dikken <olivierdikken@hotmail.fr>
2018-12-11 11:57:02 +0100

    Total Refugees plot to justify non-linear color scale

---
+++ Python/totalRefugeesPlot.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = "../WorldMap/resources/dataset_v2.csv"
+
+group = pd.read_csv(path).groupby(["Country"]).apply(lambda x: x.drop('Country', axis = 1).to_dict(orient='records')).to_dict()
+refTotal = []
+for key, value in group.items():
+    for item in value:
+        indicator = item["Indicator"]
+        plotValues = [0 if pd.isnull(i) else i for i in list(item.values())]
+        plotValues.pop(0)
+        if indicator == 'Refugees_Total':
+            refTotal.extend(plotValues)
+
+refTotal.sort()
+plt.plot(refTotal, 'ro')
+plt.ylabel('Total Refugees')
+plt.gca().axes.get_xaxis().set_visible(False)
+plt.title('Samples of total refugees per country per year in ascending order')
+plt.savefig('TotalRefugeesSamples.png', bbox_inches='tight')
+
+
+print('Finished')
