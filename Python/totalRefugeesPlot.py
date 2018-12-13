27a1d0a6767c496c5891262b9fcd8ce7ae423a27
olivier-dikken <olivierdikken@hotmail.fr>
2018-12-13 11:45:27 +0100

    Added hist of total ref for all country/year combinations
    also changed incorrect data naming in scatterplot function

--- Python/totalRefugeesPlot.py
+++ Python/totalRefugeesPlot.py
@@ -10,15 +10,24 @@ for key, value in group.items():
         indicator = item["Indicator"]
         plotValues = [0 if pd.isnull(i) else i for i in list(item.values())]
         plotValues.pop(0)
+        plotValues = filter(lambda a: a != 0, plotValues)
         if indicator == 'Refugees_Total':
             refTotal.extend(plotValues)
 
 refTotal.sort()
-plt.plot(refTotal, 'ro')
-plt.ylabel('Total Refugees')
-plt.gca().axes.get_xaxis().set_visible(False)
-plt.title('Samples of total refugees per country per year in ascending order')
-plt.savefig('TotalRefugeesSamples.png', bbox_inches='tight')
+n, bins, patches = plt.hist(refTotal, 100)
+plt.savefig('TotalRefugeesSamplesHist.png', bbox_inches='tight')
 
+#
+# plt.plot(refTotal)
+
+# plt.plot(refTotal, 'ro')
+# plt.ylabel('Total Refugees')
+# plt.gca().axes.get_xaxis().set_visible(False)
+# plt.title('Samples of total refugees per country per year in ascending order')
+# plt.savefig('TotalRefugeesSamples.png', bbox_inches='tight')
+
+
+#
 
 print('Finished')
